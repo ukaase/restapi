62909201c0c1b8d9f91cb6837e7f3897f455f6cb
illya <champzcsgo@gmail.com>
2022-07-15 02:13:20 -0300

    routes

--- cmd/server.go
+++ cmd/server.go
@@ -1,19 +1,36 @@
 package main
 
-import("fmt")
+import (
+	"fmt"
+	"net/http"
 
+	transportHTTP "github.com/ukaase/internal/transport/mizera"
+)
 
-type app struct{}
+// App - the struct which contains things like pointers
+// to database connections
+type App struct{}
 
-func(app *app) Run()error{
-   fmt.Println("setting up")
-   return nil
+// Run - sets up our application
+func (app *App) Run() error {
+	fmt.Println("Setting Up Our APP")
 
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+
+	return nil
 }
 
-func main(){
-   app:= app{}
-   if err:= app.Run;err!=nil{
-      return fmt.Println("error")
-   }
+func main() {
+	fmt.Println("Go REST API Course")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
