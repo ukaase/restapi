b7017866346856a395e131114386bcde4f6cc867
illya <champzcsgo@gmail.com>
2022-07-15 00:34:26 -0300

    structs

--- cmd/server.go
+++ cmd/server.go
@@ -2,6 +2,18 @@ package main
 
 import("fmt")
 
+
+type app struct{}
+
+func(app *app) Run()error{
+   fmt.Println("setting up")
+   return nil
+
+}
+
 func main(){
-   
+   app:= app{}
+   if err:= app.Run;err!=nil{
+      return fmt.Println("error")
+   }
 }
